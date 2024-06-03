11b1e7cddacd36b43c1dda3d74491249f2497cd6
kimhyun <forhn37@gmail.com>
2024-06-03 22:46:03 +0900

    feat : 변경사항 수정

--- app/ui/dashboard/imageslide.tsx
+++ app/ui/dashboard/imageslide.tsx
@@ -5,23 +5,23 @@ import { useState, useEffect } from "react";
 export default function Imageslide() {
   const imagelist = ['eventimage1', 'eventimage2', 'eventimage3', 'eventimage4', 'eventimage5']
 
-  const [slideindex, setSlideindex] = useState('0')
-  const interval = setInterval(()=> {
-    const newindex = setSlideindex(slideindex +1)
-  },5000)
+  // const [slideindex, setSlideindex] = useState('0')
+  // const interval = setInterval(()=> {
+  //   const newindex = setSlideindex(slideindex +1)
+  // },5000)
 
-  useEffect(()=> {
-    setInterval(()=> {
-      const newindex = setSlideindex(slideindex +1)
-    },5000)
-  },[])
-  console.log(slideindex)
+  // useEffect(()=> {
+  //   setInterval(()=> {
+  //     const newindex = setSlideindex(slideindex +1)
+  //   },5000)
+  // },[])
+  // console.log(slideindex)
   return (
     <div className="flex">
       {imagelist.map((image, index) => (
         <Image
           key={index}
-          src={`http://localhost:3000/${image}.jpg`}
+          src={`/${image}.jpg`}
           width={500}
           height={300}
           alt="Picture of the author"
