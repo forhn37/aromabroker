8716baa3354fa46dcfdc125732b1f0d352230434
kimhyun <forhn37@gmail.com>
2024-06-03 16:11:27 +0900

    feat : userouter 사용

--- app/phonemenu/about/page.tsx
+++ app/phonemenu/about/page.tsx
@@ -1,11 +1,16 @@
+'use client'
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
 
 export default function Mobilemenu() {
+  const router = useRouter();
+
 
   return (
     <div>
       <div className="flex justify-end pr-3">
-        <button className="block">BACK</button>
+        <button className="block" onClick={router.back}>BACK</button>
       </div>
       <h1 className="text-4xl m-5">ABOUT</h1>
       <div className="list-none text-3xl m-5">
