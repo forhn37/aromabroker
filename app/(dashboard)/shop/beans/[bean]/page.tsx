a92c5be8dc18b4f5562328aff9d3dc659f633923
kimhyun <forhn37@gmail.com>
2024-06-11 17:35:52 +0900

    Error : 프로퍼티 부재 foundbean이라는 배열에서 속성을 찾아야하는데 못찾음 다시봐야할듯

--- app/(dashboard)/shop/beans/[bean]/page.tsx
+++ app/(dashboard)/shop/beans/[bean]/page.tsx
@@ -1,15 +1,31 @@
+import Image from 'next/image';
 export default async function Beans({ params }: { params: { bean: string } }) {
 
   const res = await fetch('http://localhost:3000/data.json/')
   // console.log(res)
   const data = await res.json()
- 
+  console.log(data)
+  const foundBean = data.beans.find(bean => bean.name === params.bean);
+  console.log(foundBean)
+
   return (
-    <main>
-      {params.bean}
-      {data.beans[0].name}
-      {data.beans[0].price}
-      {data.beans[0].weight}
+    <main className='flex flex-col justify-center items-center bg-slate-300 p-3'>
+      <Image src={`/${params.bean}.jpg`} width={380} height={200} alt="beans" className='outline outline-black' />
+      <div className='flex flex-col items-start w-full p-4'>
+        <div className='text-3xl mb-3'>기본 정보</div>
+        <div className='text-xl mb-2'>{foundBean.name}</div>
+        <div className='text-lg mb-2'>{foundBean.price}</div>
+        <div className='text-lg mb-2'>{foundBean.weight}</div>
+        <div>배송비 : {foundBean.shippingcost}</div>
+      </div>
     </main>
   );
 }
+
+
+// "name": "starbuck2",
+// "description": "A species of legume native to East Asia.",
+// "price": 4.99,
+// "weight": "250g",
+// "shippingcost": 2.49,
+// "strength": "light"
