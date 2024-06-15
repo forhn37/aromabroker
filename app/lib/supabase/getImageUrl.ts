151ea5cfecb798e60ac43d9a192778021b8dc90c
kimhyun <forhn37@gmail.com>
2024-06-15 11:31:49 +0900

    feat : getImageUrl 에러해결 에러객체가 발생하지 않으나 에러객체를 반환하는 것을 넣어 빌드시 문제발생

--- app/lib/supabase/getImageUrl.ts
+++ app/lib/supabase/getImageUrl.ts
@@ -2,14 +2,10 @@
 import { supabase } from './supabaseClient'
 
 export async function getImageUrl(filePath: string): Promise<string | null> {
-  const { data, error } = supabase.storage
+  const { data } = supabase.storage
     .from('aromabrokerbucket')
     .getPublicUrl(filePath)
 
-  if (!data || error) {
-    console.error('Error getting public URL:', error)
-    return null
-  }
 
   return data.publicUrl;
 }
