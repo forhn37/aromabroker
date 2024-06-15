dc614542a8eada3f681ee48779ea1dfdb6fb11a3
kimhyun <forhn37@gmail.com>
2024-06-15 14:31:57 +0900

    feat : 타입가드 설정

--- app/(dashboard)/shop/dripbag/page.tsx
+++ app/(dashboard)/shop/dripbag/page.tsx
@@ -5,10 +5,12 @@ export default async function Dripbag() {
   const confirmedurls = await SupabaseGetUrls("dripbag");
   console.log(confirmedurls)
 
+   // 타입 가드를 사용하여 undefined 처리
+   const safeConfirmedUrls = confirmedurls ?? []; // undefined인 경우 빈 배열로 대체
 
   return (
     <main className="overflow-hidden w-screen">
-      <Card category={"Drip Bag"} description={"아로마브로커의 즐거움을 집에서도 느껴보세요"} confirmedurl={confirmedurls}/>
+      <Card category={"Drip Bag"} description={"아로마브로커의 즐거움을 집에서도 느껴보세요"} confirmedurl={safeConfirmedUrls}/>
       
     </main>
   );
