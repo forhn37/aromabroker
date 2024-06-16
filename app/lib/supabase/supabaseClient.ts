3904e45847969b8e6beef71fc64212f32179ed1f
kimhyun <forhn37@gmail.com>
2024-06-17 03:29:47 +0900

    feat : 환경변수 누락시 콘솔찍히는 코드 추가

--- app/lib/supabase/supabaseClient.ts
+++ app/lib/supabase/supabaseClient.ts
@@ -4,5 +4,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
+if (!supabaseUrl || !supabaseKey) {
+  console.error('Missing Supabase URL or Key');
+  throw new Error('Missing Supabase URL or Key');
+}
+
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
