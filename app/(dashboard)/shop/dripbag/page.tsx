6969b35683a4c3e0e57dafbba554aa7659b630ba
kimhyun <forhn37@gmail.com>
2024-06-17 01:55:01 +0900

    feat : dripbag 페이지도 수정

--- app/(dashboard)/shop/dripbag/page.tsx
+++ app/(dashboard)/shop/dripbag/page.tsx
@@ -2,18 +2,18 @@ import Card from "@/app/ui/dashboard/card";
 import SupabaseGetUrls from "@/app/lib/supabasegeturls";
 import { supabase } from "@/app/lib/supabase/supabaseClient";
 import GetTable from '@/app/lib/supabase/gettable';
+import { Dripbag } from '@/app/types/types';
 
-export default async function Dripbag() {
+export default async function Dripbags() {
   const confirmedurls = await SupabaseGetUrls("dripbag");
   // 타입 가드를 사용하여 undefined 처리
   const safeConfirmedUrls = confirmedurls ?? []; // undefined인 경우 빈 배열로 대체
 
-  const GetedTable = await GetTable('dripbags')
-  console.log(GetedTable)
+  const dripbagsTable = await GetTable<Dripbag>("dripbags");
 
   return (
     <main className="overflow-hidden w-screen">
-      <Card category={"Drip Bag"} description={"아로마브로커의 즐거움을 집에서도 느껴보세요"} confirmedurls={safeConfirmedUrls} datatable={GetedTable} />
+      <Card category={"Drip Bag"} description={"아로마브로커의 즐거움을 집에서도 느껴보세요"} datatables={dripbagsTable} />
 
     </main>
   );
