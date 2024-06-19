da62f7bee2880558e7efc4b9b68e38d62f5466a9
kimhyun <forhn37@gmail.com>
2024-06-19 10:53:02 +0900

    feat : card bar 형태로 변경

--- app/ui/dashboard/card.tsx
+++ app/ui/dashboard/card.tsx
@@ -1,7 +1,4 @@
 'use client'
-/**1. page에서 전달된 props인 category를 통하여 서버로부터 데이터를 받아온다
- * 2. 그 데이터 중에서  */
-
 import { useState, useEffect, useRef } from "react";
 import Carddetail from "./carddetail";
 import { Categories } from '@/app/types/types';
@@ -9,22 +6,20 @@ import { usePathname } from 'next/navigation'
 import { Bean } from "@/app/types/types";
 import Image from "next/image";
 
-
 export default function Card({ category, description, datatables, imagename }: Categories) {
   const [translateX, setTranslateX] = useState(0);
+  const [hoveringPrev, setHoveringPrev] = useState(false);
+  const [hoveringNext, setHoveringNext] = useState(false);
   const cardRef = useRef<HTMLDivElement>(null);
   const pathname = usePathname()
   const parts = pathname.split('/');
   const lastPart = parts.pop();
 
-
   const prvbutton = function () {
-    // 이전 버튼 클릭 시 container가 왼쪽으로 100vw만큼 이동합니다.
     if (translateX < 0) {
       const newTranslateX = translateX + 50;
       setTranslateX(newTranslateX);
       if (cardRef.current !== null) {
-
         cardRef.current.style.transition = 'transform 1s ease-in-out';
         cardRef.current.style.transform = `translateX(${newTranslateX}vw)`;
       }
@@ -35,12 +30,10 @@ export default function Card({ category, description, datatables, imagename }: C
     const newTranslateX = translateX - 50;
     if (cardRef.current !== null) {
       if (newTranslateX < -(50 * (datatables.length - 2))) {
-        // 마지막 슬라이드 이후에는 처음으로 돌아갑니다.
         setTranslateX(0);
         cardRef.current.style.transition = 'none';
         cardRef.current.style.transform = `translateX(0vw)`;
 
-        // 트랜지션 효과를 위해 짧은 시간 후에 다시 설정
         setTimeout(() => {
           if (cardRef.current !== null) {
             cardRef.current.style.transition = 'transform 1s ease-in-out';
@@ -54,17 +47,13 @@ export default function Card({ category, description, datatables, imagename }: C
     }
   };
 
-
   useEffect(() => {
     const interval = setInterval(() => {
-      // 자동으로 다음 슬라이드로 이동
       nextbutton();
-    }, 5000); // 5초마다 실행
+    }, 5000);
 
-    // 컴포넌트 언마운트 시 인터벌 정리
     return () => clearInterval(interval);
   }, [translateX]);
-
 
   return (
     <div className="overflow-hidden mt-3">
@@ -82,15 +71,22 @@ export default function Card({ category, description, datatables, imagename }: C
       <div style={{ width: `${50 * datatables.length}vw` }} className="flex" ref={cardRef}>
         {datatables.map((item, index) => (
           <Carddetail key={index} item={item as Bean} />
-
-        ))
-        }
-
+        ))}
       </div>
-      <div className="flex justify-around w-screen mt-4">
-        <button className="flex justify-center" onClick={prvbutton}>이전</button>
-        <button className="flex justify-center" onClick={nextbutton}>다음</button>
+      <div className="flex justify-center w-screen mt-4 ">
+        <div
+          className={`w-4 h-2 mr-2 rounded-md bg-gray-300 ${hoveringPrev ? 'w-8' : 'w-4'} transition-all duration-200`}
+          onMouseEnter={() => setHoveringPrev(true)}
+          onMouseLeave={() => setHoveringPrev(false)}
+          onClick={prvbutton}
+        />
+        <div
+          className={`w-4 h-2 rounded-md bg-gray-300 ${hoveringNext ? 'w-8' : 'w-4'} transition-all duration-200`}
+          onMouseEnter={() => setHoveringNext(true)}
+          onMouseLeave={() => setHoveringNext(false)}
+          onClick={nextbutton}
+        />
       </div>
     </div>
-  )
+  );
 }
