0a3cfa3ca30f1224d724eaa1ba05538137b21a44
kimhyun <forhn37@gmail.com>
2024-06-23 18:30:10 +0900

    feat : passHref 활용

--- app/ui/community/recipecard.tsx
+++ app/ui/community/recipecard.tsx
@@ -4,14 +4,18 @@ import { RecipeTable } from '@/app/types/types';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRecipeContext } from '@/app/lib/mycontext';
+import { useRouter } from 'next/navigation';
 
 export default function RecipeCard({ title, created_at, content, imageurl, description, coffeePublicName }: RecipeTable) {
   const { setContent } = useRecipeContext();
-  
-  const handleClick = () => {
+  const router = useRouter();
+
+  const handleClick = (e) => {
+    e.preventDefault(); // 기본 링크 동작을 막음
     setContent(content);
+    router.push(`/community/recipes/${coffeePublicName}`); // 수동으로 페이지 이동
   };
-  
+
   console.log(content)
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white m-4">
@@ -27,8 +31,8 @@ export default function RecipeCard({ title, created_at, content, imageurl, descr
         <div className="text-xl mb-2">{description}</div>
         <div className="text-gray-700 text-base py-2 flex justify-between">
           {new Date(created_at).toLocaleDateString()}
-          <Link href ={`/community/recipes/${coffeePublicName}`} >
-            <div onClick={handleClick}>자세히보기</div>
+          <Link href={`/community/recipes/${coffeePublicName}`} passHref>
+            <a onClick={handleClick}>자세히보기</a>
           </Link>
         </div>
       </div>
