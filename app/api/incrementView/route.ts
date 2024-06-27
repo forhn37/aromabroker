e5cc6e5d3488fa32c2afeff37a8fad1001dc0e31
kimhyun <forhn37@gmail.com>
2024-06-27 17:57:29 +0900

    feat  : 중간 저장

--- app/api/incrementView/route.ts
+++ app/api/incrementView/route.ts
@@ -1,15 +1,26 @@
 // app/api/incrementView/route.ts
 import { NextResponse } from 'next/server';
 import { supabase } from '@/app/lib/supabase/supabaseClient';
+export interface reqs {
+  id : number;
+  tablename : string;
+}
 
 export async function POST(request: Request) {
-  const { id, tablename } = await request.json();
+  const { id, tablename } : reqs = await request.json();
 
   if (!id || !tablename) {
     return NextResponse.json({ error: 'Missing id or tablename' }, { status: 400 });
   }
 
-  // 현재 조회수를 가져옵니다.
+  // 프로시저 호출
+  const { error } = await supabase.rpc('increment_view_count', { id, tablename });
+
+  if (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+
+  // 최신 조회수 가져오기
   const { data: currentData, error: currentError } = await supabase
     .from(tablename)
     .select('views')
@@ -20,17 +31,5 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: currentError.message }, { status: 500 });
   }
 
-  const currentViews = currentData.views;
-
-  // 조회수를 1 증가시킵니다.
-  const { data, error } = await supabase
-    .from(tablename)
-    .update({ views: currentViews + 1 })
-    .eq('id', id);
-
-  if (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
-  }
-
-  return NextResponse.json({ views: currentViews + 1 }, { status: 200 });
+  return NextResponse.json({ views: currentData.views }, { status: 200 });
 }
