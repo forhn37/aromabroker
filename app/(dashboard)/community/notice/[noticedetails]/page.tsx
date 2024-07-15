cd3f0126bdec254d0f06593edcf4b836e92802a2
kimhyun <forhn37@gmail.com>
2024-07-15 15:01:53 +0900

    feat : 삭제버튼 작성

--- app/(dashboard)/community/notice/[noticedetails]/page.tsx
+++ app/(dashboard)/community/notice/[noticedetails]/page.tsx
@@ -34,9 +34,15 @@ export default async function NoticeDetails({ params, searchParams }: BoardArg) 
   const postdata = posts.length > 0 ? posts[0] : null;
 
   return (
-    <main className="max-w-4xl mx-auto p-4 bg-neutral-100 pt-10 pb-10">
-      <div className="text-3xl text-center mb-6">{boardtitle}</div>
-      <BoardArticle post={postdata} />
-    </main>
+    <div>
+      <main className="max-w-4xl mx-auto p-4 bg-neutral-100 pt-10 pb-10">
+        <div className="text-3xl text-center mb-6">{boardtitle}</div>
+        <BoardArticle post={postdata} />
+      </main>
+      <div className="flex w-full justify-center p-2">
+        <div className="p-1 outline outline-1 mr-2">수정</div>
+        <div className="p-1 outline outline-1">삭제</div>
+      </div>
+    </div>
   );
 }
