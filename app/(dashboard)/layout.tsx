079b79befb83627b9cbd4857db26a33af545eed2
kimhyun <forhn37@gmail.com>
2024-07-23 18:09:28 +0900

    feat : 반응형 홈페이지로 변경중

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -12,18 +12,30 @@ export default function Layout({
 }>) {
   return (
     <>
-      <header className="flex justify-between p-4 sm:p-8">
-        <Link href="/">
-          <div className="relative w-20 h-14 sm:w-40 sm:h-28">
-            <Image 
+      <header className="flex justify-between p-4 sm:p-8 w-screen">
+        <div className="hidden sm:flex sm:justify-around sm:w-1/3 sm:items-center">
+          <span className="p-3 text-3xl">about</span>
+          <span className="p-3 text-3xl">business</span>
+          <span className="p-3 text-3xl">cafemenu</span>
+          <span className="p-3 text-3xl">class</span>
+          <span className="p-3 text-3xl">community</span>
+        </div>
+        <div className="relative w-20 h-14 sm:w-1/3 sm:h-28 sm:flex sm:justify-center">
+          <Link href="/">
+            <Image
               src="https://qblzpggxwtpkzdhsxtxh.supabase.co/storage/v1/object/public/aromabrokerbucket/public/mainlogo.png?t=2024-07-23T06%3A54%3A37.900Z"
-              alt="logo" 
-              fill 
-              style={{ objectFit: "contain" }} 
+              alt="logo"
+              fill
+              style={{ objectFit: "contain" }}
             />
-          </div>
-        </Link >
+          </Link >
+        </div>
+        {/* Nav는 sm이상일때는 보이지 않습니다. */}
         <Nav />
+        <div className="hidden sm:flex sm:justify-end sm:w-1/3 sm:items-center">
+          <span className="p-3 text-3xl mr-5">Board</span>
+          <span className="p-3 text-3xl">Login</span>
+        </div>
       </header>
       <RecipeProvider>
         {children}
