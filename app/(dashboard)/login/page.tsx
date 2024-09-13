04340b4d975f574e98b8a9edc7f5ab19b5edb18d
kimhyun <forhn37@gmail.com>
2024-09-13 18:02:37 +0900

    feat : 로그인화면 웹화면에 맞게 폭 수정

--- app/(dashboard)/login/page.tsx
+++ app/(dashboard)/login/page.tsx
@@ -1,54 +1,9 @@
-'use client';
+import SignIn from "@/app/ui/loginnout/signin";
 
-import { useEffect, useState } from "react";
-import { supabase } from "@/app/lib/supabase/supabaseClient";
-import { Session } from "@supabase/supabase-js";
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useRouter } from "next/navigation";
-
-export default function SignInPage() {
-  const [session, setSession] = useState<Session | null>(null);
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkLogin = async () => {
-      const { data, error } = await supabase.auth.getSession();
-      if (error) {
-        console.error("Error getting session:", error);
-      } else {
-        setSession(data.session);
-      }
-    };
-
-    checkLogin();
-
-    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
-      setSession(session);
-      if (session) {
-        router.push('/'); // 로그인 성공 시 메인 페이지로 리디렉션
-      }
-    });
-
-    return () => {
-      authListener.subscription.unsubscribe();
-    };
-  }, []);
-
+export default function LoginPage() {
   return (
-    <div className="p-5">
-      <h1 className="text-center text-3xl mb-10">Login</h1>
-      {!session ? (
-        <Auth
-          supabaseClient={supabase}
-          providers={['google', 'kakao']}
-          appearance={{ theme : ThemeSupa }}
-        />
-      ) : (
-        <div>
-          <p>Logged in as {session.user?.email}</p>
-        </div>
-      )}
+    <div className="sm:flex sm:w-full sm:justify-center">
+      <SignIn />
     </div>
-  );
+  )
 }
---
+++ app/ui/loginnout/signin.tsx
@@ -0,0 +1,54 @@
+'use client';
+
+import { useEffect, useState } from "react";
+import { supabase } from "@/app/lib/supabase/supabaseClient";
+import { Session } from "@supabase/supabase-js";
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+import { useRouter } from "next/navigation";
+
+export default function SignIn() {
+  const [session, setSession] = useState<Session | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const checkLogin = async () => {
+      const { data, error } = await supabase.auth.getSession();
+      if (error) {
+        console.error("Error getting session:", error);
+      } else {
+        setSession(data.session);
+      }
+    };
+
+    checkLogin();
+
+    const { data: authListener } = supabase.auth.onAuthStateChange((event, session) => {
+      setSession(session);
+      if (session) {
+        router.push('/'); // 로그인 성공 시 메인 페이지로 리디렉션
+      }
+    });
+
+    return () => {
+      authListener.subscription.unsubscribe();
+    };
+  }, []);
+
+  return (
+    <div className="p-5 sm:w-1/4">
+      <h1 className="text-center text-3xl mb-10">Login</h1>
+      {!session ? (
+        <Auth
+          supabaseClient={supabase}
+          providers={['google', 'kakao']}
+          appearance={{ theme: ThemeSupa }}
+        />
+      ) : (
+        <div>
+          <p>Logged in as {session.user?.email}</p>
+        </div>
+      )}
+    </div>
+  );
+}
