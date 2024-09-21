d48d80a05cb3d0d5bf402199ab138252f547ec11
kimhyun <forhn37@gmail.com>
2024-09-21 15:02:46 +0900

    feat : 일부 이벤트 페이지 구조 형성 앞으로 할것: 오른쪽 메뉴 설정 및 링크 설정

--- app/(dashboard)/eventpage/[event]/page.tsx
+++ app/(dashboard)/eventpage/[event]/page.tsx
@@ -26,25 +26,31 @@ export default async function Event({ params, searchParams }: paramsProps) {
   const imageUrl = eventDetails[ismobileweb as keyof MaineventProps] as string;
 
   return (
-    <div className="sm:flex sm:justify-around sm: p-12">
-      <div className="sm:flex sm:justify-center sm: items-start">
-        {eventDetails && eventDetails.webimageurl || eventDetails.mobileimageurl ? (
-          <Image
-            src={imageUrl}
-            width={2400}
-            height={1200}
-            alt={`Slide`}
-            className="w-full"
-          />
-        ) : (
-          <p>Image not available</p> // 대체 텍스트 또는 요소
-        )}
+    <div className="sm:flex sm:justify-around sm: p-6 ">
+      <div className="sm:flex sm:justify-center sm: items-start sm:flex-col">
+        {/* {eventDetails && eventDetails.webimageurl || eventDetails.mobileimageurl ? ( */}
+        <Image
+          src={imageUrl}
+          width={3000}
+          height={1200}
+          alt={`Slide`}
+          className="w-full"
+        />
+        <Image
+          src='/cuppingclass.jpg'
+          width={2400}
+          height={1200}
+          alt={`Slide`}
+          className="w-full sm:p-10" />
+        {/* // ) : (
+        //   <p>Image not available</p> // 대체 텍스트 또는 요소
+        // )} */}
       </div>
       {eventDetails ? (
-        <article className="p-2 sm:p-10">
+        <article className="p-2 sm:p-10 sm:w-2/5 sm:flex sm:justify-center">
           <h1 className="text-xl mb-2">{eventDetails.title}</h1>
-          <div className="whitespace-pre-wrap">
-            {eventDetails.content}
+          <div>
+            hi
           </div>
         </article>
       ) : (
