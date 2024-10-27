0e567015947326ca7f12cd10c301fb34377858a7
kimhyun <forhn37@gmail.com>
2024-10-27 13:17:55 +0900

    feat : eventimage 변경

--- app/(dashboard)/eventpage/[event]/page.tsx
+++ app/(dashboard)/eventpage/[event]/page.tsx
@@ -40,15 +40,15 @@ export default async function Event({ params, searchParams }: paramsProps) {
           <h1 className="sm:text-3xl mb-2 font-semibold">{eventDetails.title}</h1>
           <h2 className="text-2xl sm:mt-4">이달의 클래스</h2>
           <h2 className="text-2xl sm:mt-4">30,000원</h2>
-          <div className="flex flex-col sm:text-xl sm:mt-4 mt-2">
+          <div className="flex flex-col sm:text-base sm:mt-4 mt-2">
             <div>클래스 수강료는 현장결제 및 계좌이체만 가능한 점 양해 부탁드립니다. </div>
             <div>
               클래스 날짜 및 시간은 당월 인원의 스케쥴에 따라 달라질수 있습니다.
             </div>
           </div>
-          <div className="sm:mt-10 sm:w-full flex justify-center items-center sm:h-20 border border-black sm:px-10 mt-5 h-10">
-            <Link href="/community/classregister " className="w-full h-full">
-              <button className="sm:text-2xl w-full h-full text-xs ">클래스신청 바로가기</button>
+          <div className="sm:mt-10 sm:w-full flex justify-center items-center sm:h-20 border border-black sm:px-10 mt-5 h-10 bg-black">
+            <Link href="/community/classregister " className="w-full h-full rounded-md">
+              <button className="sm:text-1xl text-white w-full h-full text-xs">클래스신청 바로가기</button>
             </Link>
           </div>
         </article>
